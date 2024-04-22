148488ef7edc4b643f7d34dcd846d521aec185bf
Nirosh Shetty <niroshshetty2020@gmail.com>
2024-04-22 11:51:51 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="">
+      <HeroSection />
+    </h1>
+  );
+}
